8c25870f38baf03eb639b672c75cca5afaffb770
Gio <giovannizavala82@gmail.com>
2025-06-12 14:47:46 -0500

    initial commit.

--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
@@ -2,12 +2,14 @@ package com.pluralsight.NorthwindTradersSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class NorthwindTradersSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
+		ApplicationContext context;
+		context = SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
 	}
 
 }
