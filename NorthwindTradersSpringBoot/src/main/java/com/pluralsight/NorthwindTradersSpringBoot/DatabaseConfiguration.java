5b7ffec2f9cba9473d63f19c5e84eb315f671fd9
Gio <giovannizavala82@gmail.com>
2025-06-17 14:43:31 -0500

    exercise 4 completed.

--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/DatabaseConfiguration.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/DatabaseConfiguration.java
@@ -1,4 +1,27 @@
 package com.pluralsight.NorthwindTradersSpringBoot;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
 public class DatabaseConfiguration {
+    @Value("${connectionUrl}")
+    private String connectionUrl;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(connectionUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
 }
