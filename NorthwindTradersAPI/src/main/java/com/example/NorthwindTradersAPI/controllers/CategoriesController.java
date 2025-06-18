cd436647f486a44e78b062ecf991ffdbb8bf1802
Gio <giovannizavala82@gmail.com>
2025-06-18 14:18:05 -0500

    small changes from follow through with dave.

--- NorthwindTradersAPI/src/main/java/com/example/NorthwindTradersAPI/controllers/CategoriesController.java
+++ NorthwindTradersAPI/src/main/java/com/example/NorthwindTradersAPI/controllers/CategoriesController.java
@@ -3,14 +3,12 @@ package com.example.NorthwindTradersAPI.controllers;
 import com.example.NorthwindTradersAPI.models.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("categories")
 public class CategoriesController {
     List<Category> categories = new ArrayList<>();
 
@@ -19,11 +17,11 @@ public class CategoriesController {
         categories.add(new Category(2, "Produce"));
         categories.add(new Category(3, "Meat"));
     }
-    @GetMapping
+    @GetMapping("/categories")
     public List<Category> getAllCategories() {
         return new ArrayList<>(categories);
     }
-    @GetMapping("/{id}")
+    @GetMapping("/categories/{id}")
     public Category getCategoryById(@PathVariable int id) {
         for (Category category : categories) {
             if(category.getCategoryId() == id) {
--- NorthwindTradersAPI/src/main/java/com/example/NorthwindTradersAPI/controllers/ProductController.java
+++ NorthwindTradersAPI/src/main/java/com/example/NorthwindTradersAPI/controllers/ProductController.java
@@ -3,14 +3,12 @@ package com.example.NorthwindTradersAPI.controllers;
 import com.example.NorthwindTradersAPI.models.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
 public class ProductController {
     List<Product> products = new ArrayList<>();
 
@@ -19,11 +17,11 @@ public class ProductController {
         products.add(new Product(2, "Milk", 1, 2.50));
         products.add(new Product(3, "Apples", 2, 0.50));
     }
-    @GetMapping
+    @GetMapping("/products")
     public List<Product> getAllProducts() {
         return new ArrayList<>(products);
     }
-    @GetMapping("/{id}")
+    @GetMapping("products/{id}")
     public Product getProductById(@PathVariable int id) {
         for (Product product : products) {
             if (product.getProductId() == id) {
