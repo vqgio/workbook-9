71bb0b5db378bfc15cf0775358321a6f1666f4e1
Gio <giovannizavala82@gmail.com>
2025-06-18 13:14:54 -0500

    initial commit.

---
+++ NorthwindTradersAPI/src/test/java/com/example/NorthwindTradersAPI/NorthwindTradersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NorthwindTradersAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindTradersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
