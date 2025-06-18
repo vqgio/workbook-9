2656d62bd197844119d32e30c233473a345c03f2
Gio <giovannizavala82@gmail.com>
2025-06-18 13:14:38 -0500

    example exercise.

--- api/src/main/java/com/pluralsight/api/controllers/HomeController.java
+++ api/src/main/java/com/pluralsight/api/controllers/HomeController.java
@@ -1,13 +1,16 @@
 package com.pluralsight.api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
     // this method will respond to http://localhost:8080/
-    @RequestMapping(path="/")
-    public String index() {
-        return "Hello World!";
+//    @RequestMapping(path="/")
+//    public String index(@RequestParam(defaultValue="World") String country) {
+//        return "Hello " + country + "!";
+//    }
+    @GetMapping(path="/pathvar/{country}")
+    public String helloPathVar(@PathVariable String country) {
+        return "Hello " + country + "!";
     }
 }
