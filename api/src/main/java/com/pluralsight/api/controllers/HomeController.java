f59911ff95977622ebe60cbd3485d9403080b0bb
Gio <giovannizavala82@gmail.com>
2025-06-18 10:46:02 -0500

    Home controller.

---
+++ api/src/main/java/com/pluralsight/api/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.pluralsight.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    // this method will respond to http://localhost:8080/
+    @RequestMapping(path="/")
+    public String index() {
+        return "Hello World!";
+    }
+}
