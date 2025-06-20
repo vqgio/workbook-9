0ae96c4eeda5e51954afb5a33e3fafc103c6e703
Gio <giovannizavala82@gmail.com>
2025-06-20 11:09:06 -0500

    exercise 3 start.

---
+++ NorthwindTradersAPI/src/main/java/com/example/NorthwindTradersAPI/config/DatabaseConfiguration.java
@@ -0,0 +1,4 @@
+package com.example.NorthwindTradersAPI.config;
+
+public class DatabaseConfiguration {
+}
---
+++ NorthwindTradersAPI/src/main/java/com/example/NorthwindTradersAPI/dao/ProductDao.java
@@ -0,0 +1,4 @@
+package com.example.NorthwindTradersAPI.dao;
+
+public class ProductDao {
+}
